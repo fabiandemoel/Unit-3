8cccf358e748acb08e808ec04cb7b8b1079ab422
Fabian <fabiandemoel@gmail.com>
2018-11-14 13:43:37 +0100

    Initial Commit

---
+++ PersonalityQuiz/IntroductionViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Fabian de Moel on 12/11/2018.
+//  Copyright © 2018 Fabian de Moel. All rights reserved.
+//
+
+import UIKit
+
+class IntroductionViewController: UIViewController {
+
+    @IBAction func unwindToQuizIntroduction(segue: UIStoryboardSegue) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ PersonalityQuiz/ResultsViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ResultsViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Fabian de Moel on 12/11/2018.
+//  Copyright © 2018 Fabian de Moel. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    @IBOutlet weak var resultAnswerLabel: UILabel!
+    @IBOutlet weak var resultDefinitionLabel: UILabel!
+    
+    var responses: [Answer]!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        calculatePersonalityResult()
+        navigationItem.hidesBackButton = true
+    }
+    
+    func calculatePersonalityResult() {
+        var frequencyOfAnswers: [AnimalType: Int] = [:]
+        let responseTypes = responses.map { $0.type }
+        
+        for response in responseTypes {
+            frequencyOfAnswers[response] = (frequencyOfAnswers[response]
+        ?? 0) + 1
+        }
+        
+        let mostCommonAnswer = frequencyOfAnswers.sorted { $0.1 > $1.1 }.first!.key
+        
+        resultAnswerLabel.text = "You are a \(mostCommonAnswer.rawValue)!"
+        resultDefinitionLabel.text = mostCommonAnswer.definition
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
